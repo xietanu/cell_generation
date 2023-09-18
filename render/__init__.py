de7a07e56debfa9f21f0761f102180fdf6fda424
xietanu <100538668+xietanu@users.noreply.github.com>
2023-09-18 15:22:54 +0200

    Remove render module as using matplotlib voxel

--- render/__init__.py
+++
@@ -1,3 +0,0 @@
-from render.silhouette import silhouette
-from render.depth_map import depth_map
-from render.lit import lit
--- render/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Utility functions for rendering."""
-from render.utils.create_normal_map import create_normal_map
-from render.utils.create_background_mask import create_background_mask
