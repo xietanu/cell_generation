a7f9276fd71dd6bb5691927d54e21f0877684929
xietanu <100538668+xietanu@users.noreply.github.com>
2023-09-22 10:16:51 +0200

    Add new image datasets to init

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,3 +2,5 @@
 from datasets import synth
 from datasets.auto_img import AutoImg
 from datasets.auto_img_cached import AutoImgCached
+from datasets.cached_images import CachedImages, split_folder
+from datasets.images import Images
