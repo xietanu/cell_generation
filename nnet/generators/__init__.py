e350df945ee28015c14b17163c3dea357baa1d0f
xietanu <100538668+xietanu@users.noreply.github.com>
2023-09-22 10:14:38 +0200

    Update init to reflect changes to handling of generators

--- nnet/generators/__init__.py
+++ nnet/generators/__init__.py
@@ -1,11 +1,2 @@
 from nnet.generators.vae import VAE, vae_loss_function
-from nnet.generators.solid_mask_generator import SolidMaskGenerator
-from nnet.generators.trans_mask_generator import TransMaskGenerator
-from nnet.generators.trans_gray_mask_generator import TransGrayMaskGenerator
-from nnet.generators.trans_mask_generator_ii import TransMaskGeneratorII
 
-# from nnet.generators.mask_encoder import MaskEncoder
-# from nnet.generators.mask_discriminator import MaskDiscriminator
-# from nnet.generators.mask_voxel_generator import MaskVoxelGenerator
-# from nnet.generators.colour_voxel_generator import ColourVoxelGenerator
-# from nnet.generators.trans_mask_voxel_generator import TransMaskVoxelGenerator
