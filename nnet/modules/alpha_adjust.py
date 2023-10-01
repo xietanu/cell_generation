Correct alpha adjust formula and add random patch module

nnet/modules/alpha_adjust.py
@@ -7,7 +7,7 @@ class VoxGridAlphaAdjust(torch.nn.Module):
 
     def forward(self, in_tensor: torch.Tensor) -> torch.Tensor:
         """Adjusts the alpha values of a voxel grid to assist with training."""
-        adjust_value = np.log2(in_tensor.shape[-1]) + 3
+        adjust_value = np.log2(1 - 0.5 ** (1 / in_tensor.shape[-1])) / np.log2(0.5)
 
         values = in_tensor[:, :-1, :, :, :]
         alpha = in_tensor[:, -1, :, :, :].unsqueeze(1) ** adjust_value

nnet/modules/always_dropout.py
@@ -4,15 +4,16 @@ import torch
 class AlwaysDropout(torch.nn.Module):
     """Module that alwazs performs a convolutional dropout."""
 
-    def __init__(self, probability: float = 0.1):
+    def __init__(self, probability: float = 0.1, device: str | torch.device = "cuda"):
         super().__init__()
         self.probability = probability
+        self.device = device
 
     def forward(self, in_tensor: torch.Tensor) -> torch.Tensor:
         """Adjusts the alpha values of a voxel grid to assist with training."""
         mask = torch.bernoulli(
-            torch.ones((1, in_tensor.shape[1]) + (1,) * len(in_tensor.shape[2:]))
-            * (1 - self.probability)
-        )
+            torch.ones((in_tensor.shape[0], in_tensor.shape[1]) + (1,) * len(in_tensor.shape[2:]))
+            * (1 - self.probability),
+        ).to(self.device)
 
         return in_tensor * mask