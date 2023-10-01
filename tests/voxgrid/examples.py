848c189185df95a64ad2ea8f3ac7c604709b12ca
xietanu <100538668+xietanu@users.noreply.github.com>
2023-10-01 16:29:06 +0200

    Add new tests for getting alpha and value channels

--- tests/voxgrid/examples.py
+++ tests/voxgrid/examples.py
@@ -1,34 +1,43 @@
+from dataclasses import dataclass
+
 import numpy as np
 import torch
 
 
-def get_example_0c_voxgrid(as_tensor: bool = False):
-    if as_tensor:
-        voxels = torch.zeros((1, 3, 3, 3), dtype=torch.float32)
-    else:
-        voxels = np.zeros((3, 3, 3), dtype=np.float32)
-    voxels[..., 0, 0, 0] = 1
-    voxels[..., 0, 1, 0] = 1
-    voxels[..., 0, 1, 1] = 1
-    voxels[..., 1, 1, 1] = 0.5
-    voxels[..., 1, 0, 2] = 1
-    return voxels
+@dataclass
+class ExampleVoxelGrid:
+    array: np.ndarray
+    tensor: torch.Tensor
+    values: np.ndarray
+    alpha: np.ndarray
+
+    def get_array_without_channels(self) -> np.ndarray:
+        return self.array[0, :, :, :, -1]
+
+    def get_array_with_channels(self) -> np.ndarray:
+        return self.array[0, :, :, :, :]
+
+    def get_array_with_n(self) -> np.ndarray:
+        return self.array
+
+    def get_tensor_with_channels(self) -> torch.Tensor:
+        return self.tensor[0, :, :, :, :]
+
+    def get_tensor_with_n(self) -> torch.Tensor:
+        return self.tensor
 
 
-def get_example_1c_voxgrid(as_tensor: bool = False):
-    if as_tensor:
-        voxels = torch.zeros((3, 3, 3), dtype=torch.float32)
-    else:
-        voxels = np.zeros((3, 3, 3), dtype=np.float32)
+def get_example_1_channel_voxgrid():
+    voxels = np.zeros((3, 3, 3), dtype=np.float32)
     voxels[0, 0, 0] = 1
     voxels[0, 1, 0] = 1
     voxels[0, 1, 1] = 1
     voxels[1, 1, 1] = 0.5
     voxels[1, 0, 2] = 1
 
-    if isinstance(voxels, torch.Tensor):
-        voxels = voxels.unsqueeze(0)
-    else:
-        voxels = voxels[..., np.newaxis]
-    return voxels
+    array = voxels[np.newaxis, ..., np.newaxis].copy()
+    tensor = torch.from_numpy(voxels).unsqueeze(0).unsqueeze(0)
+    alpha = voxels.copy()
+    values = np.ones_like(alpha)
 
+    return ExampleVoxelGrid(array, tensor, values, alpha)
--- tests/voxgrid/test_get_alpha_values.py
+++ tests/voxgrid/test_get_alpha_values.py
@@ -1,8 +1,7 @@
 import pytest
-import torch
 import numpy as np
 
-from tests.voxgrid.examples import get_example_0c_voxgrid
+from tests.voxgrid.examples import get_example_1_channel_voxgrid
 from voxgrid import get_alpha, get_values
 
 
@@ -10,14 +9,25 @@ from voxgrid import get_alpha, get_values
     "voxgrid, expected",
     [
         (
-            get_example_0c_voxgrid(as_tensor=False),
-            get_example_0c_voxgrid(as_tensor=False),
+            get_example_1_channel_voxgrid().array,
+            get_example_1_channel_voxgrid().alpha[np.newaxis, ..., np.newaxis].copy(),
         ),
         (
-            get_example_0c_voxgrid(as_tensor=True),
-            get_example_0c_voxgrid(as_tensor=True).squeeze(),
+            get_example_1_channel_voxgrid().tensor.squeeze(),
+            get_example_1_channel_voxgrid().alpha,
         ),
+        (
+            get_example_1_channel_voxgrid().get_array_without_channels(),
+            get_example_1_channel_voxgrid().alpha,
+        ),
+        (
+            get_example_1_channel_voxgrid().get_array_with_channels(),
+            get_example_1_channel_voxgrid().alpha[..., np.newaxis].copy(),
+        )
     ],
 )
 def test_get_alpha(voxgrid, expected):
+    print(voxgrid.shape, expected.shape)
+    print(voxgrid.squeeze())
+    print(expected.squeeze())
     assert np.allclose(get_alpha(voxgrid), expected)
