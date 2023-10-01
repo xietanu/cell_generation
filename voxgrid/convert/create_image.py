ca6c6408d710d5e9a08d6a6484a8744b61c23f1b
xietanu <100538668+xietanu@users.noreply.github.com>
2023-10-01 16:30:35 +0200

    Add ability to supply background to image gen from vox grid

--- voxgrid/convert/create_image.py
+++ voxgrid/convert/create_image.py
@@ -5,25 +5,45 @@ import voxgrid
 
 
 def create_image(
-    voxel_grid: torch.Tensor | np.ndarray, device: torch.device = torch.device("cpu")
+    voxel_grid: torch.Tensor | np.ndarray,
+    device: torch.device = torch.device("cpu"),
+    background: torch.Tensor | np.ndarray | None = None,
 ) -> np.ndarray | torch.Tensor:
     """Returns a 2D image of the voxel model."""
 
     if isinstance(voxel_grid, np.ndarray):
-        return create_image_from_numpy(voxel_grid)
+        if isinstance(background, torch.Tensor):
+            raise ValueError(
+                "Cannot use torch tensor as background for numpy voxel grid."
+            )
+        return create_image_from_numpy(voxel_grid, background=background)
 
     if isinstance(voxel_grid, torch.Tensor):
-        return create_image_from_torch(voxel_grid, device=device)
+        if isinstance(background, np.ndarray):
+            raise ValueError(
+                "Cannot use numpy array as background for torch voxel grid."
+            )
+        return create_image_from_torch(voxel_grid, device=device, background=background)
 
 
-def create_image_from_numpy(voxel_grid: np.ndarray):
+def create_image_from_numpy(
+    voxel_grid: np.ndarray, background: np.ndarray | None = None
+):
     n_channels = voxgrid.get_n_channels(voxel_grid)
     n_images = voxel_grid.shape[0] if voxel_grid.ndim > 4 else 1
 
-    image = np.zeros(
-        (n_images, voxel_grid.shape[0], voxel_grid.shape[1], max(n_channels - 1, 1)),
-        dtype=np.float32,
-    )
+    if background is None:
+        image = np.zeros(
+            (
+                n_images,
+                voxel_grid.shape[0],
+                voxel_grid.shape[1],
+                max(n_channels - 1, 1),
+            ),
+            dtype=np.float32,
+        )
+    else:
+        image = background.copy()
 
     alphas = voxgrid.get_alpha(voxel_grid)[..., np.newaxis]
     values = voxgrid.get_values(voxel_grid)
@@ -48,15 +68,24 @@ def create_image_from_numpy(voxel_grid: np.ndarray):
 def create_image_from_torch(
     voxel_grid: torch.Tensor,
     device: torch.device = torch.device("cpu"),
+    background: torch.Tensor | None = None,
 ):
     n_channels = voxgrid.get_n_channels(voxel_grid)
     n_images = voxel_grid.shape[0] if voxel_grid.ndim > 4 else 1
 
-    image = torch.zeros(
-        (n_images, max(n_channels - 1, 1), voxel_grid.shape[2], voxel_grid.shape[3]),
-        dtype=torch.float32,
-        device=device,
-    )
+    if background is None:
+        image = torch.zeros(
+            (
+                n_images,
+                max(n_channels - 1, 1),
+                voxel_grid.shape[2],
+                voxel_grid.shape[3],
+            ),
+            dtype=torch.float32,
+            device=device,
+        )
+    else:
+        image = background.to(device)
 
     alphas = voxgrid.get_alpha(voxel_grid).unsqueeze(-4)  # type: ignore
     values = voxgrid.get_values(voxel_grid).to(device)  # type: ignore
@@ -68,7 +97,7 @@ def create_image_from_torch(
         values = values.unsqueeze(0)
         alphas = alphas.unsqueeze(0)
 
-    for level in reversed(range(voxel_grid.shape[-1])):
+    for level in range(voxel_grid.shape[-1]):
         alpha = alphas[..., level]
         value = values[..., level]
         image = image * (1 - alpha) + alpha * value
