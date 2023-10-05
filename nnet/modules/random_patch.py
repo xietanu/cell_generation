3e4eec1c38c28d7f9fee61c92a098c426110ebe3
xietanu <100538668+xietanu@users.noreply.github.com>
2023-10-06 00:31:05 +0200

    Correct docstrings

--- nnet/modules/random_patch.py
+++ nnet/modules/random_patch.py
@@ -1,16 +1,17 @@
+"""Module that randomly crops a patch of specified size from an image."""
 import torch
 import numpy as np
 
 
 class RandomPatch(torch.nn.Module):
-    """Module that alwazs performs a convolutional dropout."""
+    """Module that randomly crops a patch of specified size from an image."""
 
     def __init__(self, size: tuple[int, int]):
         super().__init__()
         self.size = size
 
     def forward(self, in_tensor: torch.Tensor) -> torch.Tensor:
-        """Adjusts the alpha values of a voxel grid to assist with training."""
+        """Crop a random patch from the input tensor."""
         if in_tensor.shape[2] == self.size[0] and in_tensor.shape[3] == self.size[1]:
             return in_tensor
         top = np.random.randint(0, in_tensor.shape[2] - self.size[0])
