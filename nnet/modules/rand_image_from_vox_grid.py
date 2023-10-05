658744f29a4d9719427255a9ca11ad74f34ad356
xietanu <100538668+xietanu@users.noreply.github.com>
2023-10-06 00:31:26 +0200

    Add informative docstrings

--- nnet/modules/rand_image_from_vox_grid.py
+++ nnet/modules/rand_image_from_vox_grid.py
@@ -4,6 +4,12 @@ import voxgrid
 
 
 class RandomImageFromVoxGrid(torch.nn.Module):
+    """Creates images from random angles from the voxel grids supplied.
+
+    By default these are layered over a black background, but a background image
+    tensor can be supplied.
+    """
+
     def __init__(self, device: torch.device = torch.device("cpu")) -> None:
         super().__init__()
         self.device = device
@@ -21,6 +27,24 @@ class RandomImageFromVoxGrid(torch.nn.Module):
     def forward(
         self, in_tensor: torch.Tensor, background: torch.Tensor | None = None
     ) -> torch.Tensor:
+        """Creates an image of the input voxel grid tensor from a random angle.
+
+        Parameters
+        ----------
+        in_tensor : torch.Tensor
+            Input voxel grid tensor. Should be of shape (N, C, H, W, D).
+        background : torch.Tensor | None, optional
+            Background image tensor for the voxel grid to be layered over.
+            Should be of shape (C', H, W, D), where C' is 1 for 1 or 2 channel voxel grids
+            and 3 for 4 channel voxel grids.
+            By default None, will use a flat black background.
+
+        Returns
+        -------
+        torch.Tensor
+            The output image tensor.
+        """
+
         rotated = voxgrid.transform.randomly_rotated(in_tensor, device=self.device)
 
         return voxgrid.convert.create_image(rotated, device=self.device, background=background)  # type: ignore
